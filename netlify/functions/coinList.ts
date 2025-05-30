a7b36c51e2141a28712bdcbeaa2694f6b38a4802
Jeremy Badger <jeremebadger@gmail.com>
2025-05-30 00:47:55 -0400

    Revert "Test"
    This reverts commit 3baf9b63b8ff6e5ef55b1dba811d95ae3f5fe151.

--- netlify/functions/coinList.ts
+++ netlify/functions/coinList.ts
@@ -27,59 +27,41 @@
 //   }
 // }
 
-// export async function handler(_event) {
-//   try {
-//     // const response = await fetch('https://cryptorates.ai/v1/coins/500')
-//     const url = `https://rest.coincap.io/v3/assets?apiKey=${process.env.REACT_APP_API_KEY}`
-//     const response = await fetch(url)
+export async function handler(_event) {
+  try {
+    // const response = await fetch('https://cryptorates.ai/v1/coins/500')
+    const url = `https://rest.coincap.io/v3/assets?apiKey=${process.env.REACT_APP_API_KEY}`
+    const response = await fetch(url)
 
-//     const rawText = await response.text()
+    const rawText = await response.text()
 
-//     let parsed
-//     try {
-//       parsed = JSON.parse(rawText)
-//     } catch (err) {
-//       return {
-//         statusCode: 500,
-//         body: JSON.stringify({
-//           error: 'Failed to parse JSON',
-//           message: rawText.slice(0, 500),
-//         }),
-//       }
-//     }
-exports.handler = async (event) => {
-  const apiKey = process.env.COINCAP_API_KEY
-  const { offset = '0', limit = '100' } = event.queryStringParameters
+    let parsed
+    try {
+      parsed = JSON.parse(rawText)
+    } catch (err) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          error: 'Failed to parse JSON',
+          message: rawText.slice(0, 500),
+        }),
+      }
+    }
 
-  const url = `https://rest.coincap.io/v3/assets?limit=${limit}&offset=${offset}`
-  const res = await fetch(url, {
-    headers: {
-      Authorization: `Bearer ${apiKey}`,
-    },
-  })
-
-  const data = await res.json()
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data),
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ data: parsed }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: 'Internal Server Error',
+        detail: error.message,
+      }),
+    }
   }
 }
-
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify({ data: parsed }),
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     }
-//   } catch (error) {
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({
-//         error: 'Internal Server Error',
-//         detail: error.message,
-//       }),
-//     }
-//   }
-// }
